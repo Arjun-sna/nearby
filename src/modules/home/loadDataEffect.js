f2ff1eda727ac9a532b39e7f4276c46be4b1703b
Arjun sn <snarjun92@gmail.com>
2019-06-27 12:22:03 +0530

    add custome api data loader hook

---
+++ src/modules/home/loadDataEffect.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import ApiService from '~/utils/apiService';
+
+export function useLoadDataFromAPI(apiPath, params = {}) {
+  const [apiData, setApiData] = useState(null);
+  const [isRequestInProgress, setIsRequestInProgress] = useState(false);
+
+  useEffect(() => {
+    async function fetchFromAPI() {
+      setIsRequestInProgress(true);
+      const apiResponseData = await ApiService.sendGet(apiPath, params);
+      setIsRequestInProgress(false);
+      setApiData(apiResponseData)
+    }
+
+    fetchFromAPI();
+  }, []);
+
+  return [isRequestInProgress, apiData];
+}
