57bcafd7dc607da92b61281b3ec811998f14ac1c
Arjun sn <snarjun92@gmail.com>
2019-06-27 18:31:46 +0530

    add styles to restaurant list item

--- src/components/restaurantListItem/index.js
+++ src/components/restaurantListItem/index.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import './styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const RestaurantListItem = ({ restaurantData }) => {
   const {
     thumb,
     name,
+    cuisines,
+    average_cost_for_two,
+    timings,
   } = restaurantData;
   
   return (
     <div className="restaurant-list-item-container">
       <div className="restaurant-list-item-cover-img">
         <img src={thumb} alt="restaurant cover"/>
+      </div>
+      <div className="restaurant-list-item-detail-container">
+        <div className="restaurant-list-item-name">{name}</div>
+        {
+          cuisines && 
+          <div className="restaurant-detail-wrapper">
+            <FontAwesomeIcon icon="utensils" size="xs" color="#00000099"  />
+            <div className="restaurant-list-item-cuisines">
+              {cuisines}
+            </div>
+          </div>
+        }
+        <div className="restaurant-detail-wrapper">
+          <FontAwesomeIcon icon="clock" size="xs" color="#00000099" />
+          <div className="restaurant-list-item-timings">
+            {timings}
+          </div>
+        </div>
+        <div className="restaurant-detail-wrapper">
+          <FontAwesomeIcon icon="rupee-sign" size="xs" color="#00000099" />
+          <div className="restaurant-list-item-cost">
+            {`${average_cost_for_two} for 2`}
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/initIcons.js
+++ src/initIcons.js
@@ -2,9 +2,15 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons/faAngleDown';
 import { faWindowClose } from '@fortawesome/free-solid-svg-icons/faWindowClose';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons/faMapMarkerAlt';
+import { faClock } from '@fortawesome/free-solid-svg-icons/faClock';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons/faUtensils';
+import { faRupeeSign } from '@fortawesome/free-solid-svg-icons/faRupeeSign';
 
 library.add(
   faAngleDown,
   faWindowClose,
   faMapMarkerAlt,
+  faClock,
+  faUtensils,
+  faRupeeSign,
 );
