4fa2ed412bad37ff7ab6d89a0aaef04f7f768304
Arjun sn <snarjun92@gmail.com>
2019-07-01 20:46:26 +0530

    detect end of data from server

--- src/modules/home/components/RestaurantsList/index.js
+++ src/modules/home/components/RestaurantsList/index.js
@@ -11,6 +11,7 @@ const RestaurantList = ({ filters }) => {
   const [startFromOffset, setStartFromOffset] = useState(0);
   const [restaurantList, setRestaurantList] = useState([]);
   const [isRequestInProgress, setIsRequestInProgress] = useState(false);
+  const [hasMoreData, setHasMoreData] = useState(true);
   const [appContextValue] = useAppContext();
   const { userLocation: { latitude, longitude } = {} } = appContextValue;
   const locationFilter = latitude && longitude ? {
@@ -27,15 +28,17 @@ const RestaurantList = ({ filters }) => {
     ...locationFilter,
   };
   const scrollEndCallback = useCallback(() => {
-    !isRequestInProgress && setStartFromOffset(startFromOffset + 20)
-  }, [startFromOffset, isRequestInProgress]);
+    (!isRequestInProgress && hasMoreData) && setStartFromOffset(startFromOffset + 20)
+  }, [startFromOffset, isRequestInProgress, hasMoreData]);
 
   useEffect(() => {
     async function fetchFromAPI() {
       setIsRequestInProgress(true);
       const apiResponseData = await ApiService.getAllRestaurants(params);
       setIsRequestInProgress(false);
-      setRestaurantList(restaurantList.concat(apiResponseData.restaurants.map(({ restaurant }) => restaurant)));
+      const nextRestaurantList = apiResponseData.restaurants.map(({ restaurant }) => restaurant);
+      nextRestaurantList.length ?
+        setRestaurantList(restaurantList.concat(nextRestaurantList)) : setHasMoreData(false);
     }
 
     fetchFromAPI();
