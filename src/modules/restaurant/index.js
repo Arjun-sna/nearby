1a75195bf52f010a66e5e73a03eb72a86a91fed6
Arjun sn <snarjun92@gmail.com>
2019-07-09 17:15:24 +0530

    add header part

--- src/modules/restaurant/index.js
+++ src/modules/restaurant/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import './styles.scss';
+import styles from './styles.scss';
 
-export default () => (
-  <div>
-    Hello world!!
-  </div>
-)
+export default ({ location }) => {
+  const { restaurantData } = location.state; 
+  console.log({restaurantData})
+  return (
+    <div className="container">
+      <div className={styles['details-header-card-container']}>
+        <img src={restaurantData.featured_image} />
+        <div className={styles['details-section']}>
+          <div className={styles['name']}>
+            { restaurantData.name }
+          </div>
+          <div className={styles['secondary-details']}>
+            { restaurantData.location.locality }
+            <span className={styles['dot']}>  ·  </span>
+            { restaurantData.establishment[0] }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
