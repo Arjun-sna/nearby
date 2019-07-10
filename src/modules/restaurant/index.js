3e6ba7e4c790efdbab51c783737c19a04c675c14
Arjun sn <snarjun92@gmail.com>
2019-07-10 17:19:07 +0530

    add restaurant additional details

--- src/modules/restaurant/index.js
+++ src/modules/restaurant/index.js
@@ -1,14 +1,58 @@
 import React from 'react';
+import { Grid, Col, Row } from 'react-flexbox-grid';
 import styles from './styles.scss';
 
+const renderRestaurantData = (title, content) => (
+  <div className={styles['detail-item-container']}>
+    <div className={styles['detail-title']}>
+      { title }
+    </div>
+    <div className={styles['detail-content']}>
+      { content }
+    </div>
+  </div>
+)
+const renderAdditionalDetails = (restaurantData) => {
+  const additionalDetails = {
+    'Cuisines': restaurantData.cuisines,
+    'Opening hours': restaurantData.timings,
+    'Average Cost': `₹ ${restaurantData.average_cost_for_two} for two`,
+    'Highlights': `${restaurantData.highlights.join(', ')}`,
+    'Phone number': `${restaurantData.phone_numbers}`,
+    'Rating': `${restaurantData.user_rating ? restaurantData.user_rating.rating_text : 'No rating'}`,
+    'Location': restaurantData.location.address,
+  };
+
+  const detailsContent = [];
+  const detailsLabels = Object.keys(additionalDetails);
+
+  for(let i = 0; i < detailsLabels.length;) {
+    detailsContent.push(
+      <Row key={`${detailsLabels[i]}-${detailsLabels[i + 1]}`}>
+        <Col xs={6}>
+          { renderRestaurantData(detailsLabels[i], additionalDetails[detailsLabels[i]]) }
+        </Col>
+        <Col xs={6}>
+          { renderRestaurantData(detailsLabels[i + 1], additionalDetails[detailsLabels[i + 1]]) }
+        </Col>
+      </Row>
+    )
+
+    i += 2;
+  }
+
+  return detailsContent;
+}
+
 export default ({ location }) => {
-  const { restaurantData } = location.state; 
+  const { restaurantData } = location.state;
+  
   console.log({restaurantData})
   return (
     <div className="container">
       <div className={styles['details-header-card-container']}>
         <img src={restaurantData.featured_image} />
-        <div className={styles['details-section']}>
+        <div className={styles['header-details-section']}>
           <div className={styles['name']}>
             { restaurantData.name }
           </div>
@@ -19,6 +63,9 @@ export default ({ location }) => {
           </div>
         </div>
       </div>
+      <Grid className={styles['details-section']}>
+        { renderAdditionalDetails(restaurantData) }
+      </Grid>
     </div>
   )
 };
